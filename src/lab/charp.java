1933653341007869172ca1f0d48932eb9d456dad
adarsh-ace <adarshsinght56@gmail.com>
2026-03-24 11:00:13 +0530

    char arr

---
+++ src/lab/charp.java
@@ -0,0 +1,19 @@
+package lab;
+
+public class charp {
+    public static void main(String[] args) {
+        char  [] s={'a','d','a','r','s','h'};
+        int st=0;
+        int en=s.length-1;
+        while(st<=en){
+            char temp=s[st];
+            s[st]=s[en];
+            s[en]= temp;
+            st++;
+            en--;
+        }
+
+        for(char i:s){
+            System.out.print(i+"#"+" ");
+        }}
+}
